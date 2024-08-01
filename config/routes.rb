040bae2c01d3e28936db87d8c4303cbebfa1744e
max <itmanselmo@gmail.com>
2024-08-01 18:31:36 -0400

    initial sessions / users setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get "users/new"
+  get "users/create"
+  # get "sessions/new"
+  # get "sessions/create"
+  # get "sessions/destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -11,4 +16,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root 'sessions#new'
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy', as: 'logout'
+
+  resources :users, only: [:new, :create]
 end
